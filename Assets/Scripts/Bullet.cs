a6d345674d53e5f691eb34235df0deed44a8af2a
Landen Love <landenoooo@gmail.com>
2021-04-01 16:04:40 -0500

    Updated Bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private float bulletLife = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletLife = Time.time + 3f;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(bulletLife - Time.time);
+        if (bulletLife <= Time.time) Destroy(gameObject);
+    }
+}
