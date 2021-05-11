63514f971381d98c15f977c77243a55d1cd8e443
Riley <rileysmith17@augustana.edu>
2021-05-11 16:50:35 -0500

    Fixed enemy attacking to attack in regular intervals. Fixed a bug where enemies would walk through objects. Found a possible solution for rotating healthbars, but it does lock all rotation.

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject goomba;
+    [SerializeField] private GameObject guard;
+    [SerializeField] private GameObject patrol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject[] generateEnemies(GameObject[] spawnPositions)
+    {
+        return null;
+    }
+
+}
