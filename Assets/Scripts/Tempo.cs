764af3792e1d94040916656bae87679af4683226
mulualem123 <55210482+mulualem123@users.noreply.github.com>
2021-05-14 21:15:05 -0500

    Speed Booster

--- Assets/Scripts/Tempo.cs
+++ Assets/Scripts/Tempo.cs
@@ -6,20 +6,17 @@ public class Tempo : MonoBehaviour
 {
 
     private GameObject playerObject;
+    private PlayerController playerMovement;
     private Health playerHealth;
     private int count = 0;
     public int howManyHealing = 4;
     public float healingPerColide;
     public float maxHealth =100;
+    public float speedBoostAmount;
 
     private void OnTriggerEnter(Collider collision)
     {
-        Debug.Log(collision.gameObject.tag);
-        if (collision.gameObject.tag == "Player") {
-            playerHealth = collision.gameObject.GetComponent<Health>();
-            playerHealth.AddHealth((int)Mathf.Min(healingPerColide + playerHealth.GetHealth(), (maxHealth)));
-
-        }    
+        Healing(collision);
     }
 
     private void OnTriggerExit(Collider collision) {
@@ -27,6 +24,24 @@ public class Tempo : MonoBehaviour
         Debug.Log(count);
         if (collision.gameObject.tag == "Player" && count == howManyHealing) {
             Destroy(this.gameObject); // Makes the health pack disappear.
+        }
+    }
+
+    // Handls the health pack
+    private void Healing( Collider collision) {
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "Player")
+        {
+            playerHealth = collision.gameObject.GetComponent<Health>();
+            playerHealth.AddHealth((int)Mathf.Min(healingPerColide + playerHealth.GetHealth(), (maxHealth)));
+
+        }
+    }
+
+    private void HealthBoost(Collider collision) {
+        if (collision.gameObject.tag == "Player") {
+            playerMovement = collision.gameObject.GetComponent<PlayerController>();
+            playerMovement.playerSpeed += speedBoostAmount;
         }
     }
 }
