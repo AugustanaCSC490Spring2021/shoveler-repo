eb281131f4b77866127454811cb2014a69a4a507
mulualem123 <55210482+mulualem123@users.noreply.github.com>
2021-05-17 17:13:58 -0500

    Score is shown on the health bar

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -7,28 +7,32 @@ public class HealthBar : MonoBehaviour
 {
     private GameObject playerObject;
     private Health playerHealth;
+    private PlayerController playerController;
 
     public Slider slider;
     public Gradient gradient;
     public Image Fill;
     public Text HealthText;
+    public Text ScoreText;
 
     private void Awake()
     {
         playerObject = GameObject.FindGameObjectWithTag("Player");
         playerHealth = playerObject.GetComponent<Health>();
+        playerController = playerObject.GetComponent<PlayerController>();
         // Landen is settings this for temp testing. We can change later if we need.
-        SetMaxHealth(playerHealth.GetHealth());
+        SetMaxHealth(playerHealth.GetHealth(), playerController.score);
     }
 
     private void FixedUpdate()
     {
+        ScoreText.text = playerController.score.ToString();
         HealthText.text = playerHealth.GetHealth().ToString();
         slider.value = playerHealth.GetHealth();
         Fill.color = gradient.Evaluate(slider.normalizedValue);
         //Debug.Log(playerHealth.GetHealth());
     }
-    public void SetMaxHealth(int maxHealth)
+    public void SetMaxHealth(int maxHealth, long score)
     {
         slider.maxValue = maxHealth;
         slider.value = maxHealth;
